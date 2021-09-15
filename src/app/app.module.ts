ce0594efc6d2dbb633103c8e0754144871a04119
rishabh <rishabhmathe4040@gmail.com>
2021-09-15 09:07:15 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpModule} from '@angular/http';  
+import { ReactiveFormsModule } from '@angular/forms';  
+import { RouterModule } from '@angular/router';  
+
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { AuthenticationService } from './services/authentication.service';
+import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path : '',  
+        component : HomeComponent
+      },
+      {
+        path : 'login',  
+        component : LoginComponent 
+      },
+      {
+        path : 'register',
+        component: RegisterComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    AuthenticationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
